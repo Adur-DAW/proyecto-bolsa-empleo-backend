26a185eb620c63aa2e55683d282df6924b0b6a40
Adur Marques <adur.marques@gmail.com>
2025-03-28 21:55:47 +0100

    Se obtiene si el usuario está inscrito también en el detalle

--- app/Http/Controllers/Api/OfertasController.php
+++ app/Http/Controllers/Api/OfertasController.php
@@ -71,11 +71,21 @@ class OfertasController extends Controller
 
     public function obtenerPorId($id)
     {
+        try {
+            $usuario = JWTAuth::parseToken()->authenticate();
+        } catch (\Exception) {
+            return response()->json(Oferta::with('empresa')->get());
+        }
+
         $oferta = Oferta::with('empresa')->find($id);
         $oferta->demandantes_inscritos = $oferta->demandantes->count();
 
         if (!$oferta) {
             return response()->json(['error' => 'Oferta no encontrada'], 404);
+        }
+
+        if ($usuario->rol === 'demandante') {
+            $oferta->inscrito = $oferta->demandantes->contains($usuario->demandante->id_demandante);
         }
 
         return response()->json($oferta);
